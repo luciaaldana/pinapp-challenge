d1fad3039203ca2bd7779869ceb307b335888022
luciaaldana <luciaaldanacastillo@gmail.com>
2025-03-01 11:27:11 -0300

    chore(init): initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  important: '#__next',
+};
